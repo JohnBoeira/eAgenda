8b066ee82a1b5e6c247db80617517e3769e56c43
johnboeira <jbiel72001@gmail.com>
2021-07-07 22:46:36 -0300

    Adicionado SQLite

---
+++ eAgenda.Tests/CompromissoModule/CompromissoTest.cs
@@ -0,0 +1,56 @@
+﻿using eAgenda.Dominio.CompromissoModule;
+using eAgenda.Dominio.ContatoModule;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eAgenda.Tests.CompromissoModule
+{
+    [TestClass]
+    public class CompromissoTest
+    {
+        [TestMethod]
+        public void DeveValidar_Assunto()
+        {
+            //arrange
+            Contato contato = new Contato("aa","aa","aa","aa","aa");
+            var compromisso = new Compromisso("","Casa","", DateTime.Now, new TimeSpan(), new TimeSpan(),contato);        
+
+            //action
+            string resultadoValidacao = compromisso.Validar();
+
+            //assert
+            resultadoValidacao.Should().Be("O campo Assunto é obrigatório");
+        }
+        [TestMethod]
+        public void DeveValidar_Data()
+        {
+            //arrange
+            Contato contato = new Contato("aa", "aa", "aa", "aa", "aa");
+            var compromisso = new Compromisso("aa", "Casa", "", DateTime.MinValue, new TimeSpan(), new TimeSpan(), contato);
+
+            //action
+            string resultadoValidacao = compromisso.Validar();
+
+            //assert
+            resultadoValidacao.Should().Be("O campo Data é obrigatório");
+        }
+        [TestMethod]
+        public void DeveValidar_TudoValido()
+        {
+            //arrange
+            Contato contato = new Contato("aa", "aa", "aa", "aa", "aa");
+            var compromisso = new Compromisso("aa", "Casa", "aaa", DateTime.Now, new TimeSpan(), new TimeSpan(), contato);
+
+            //action
+            string resultadoValidacao = compromisso.Validar();
+
+            //assert
+            resultadoValidacao.Should().Be("ESTA_VALIDO");
+        }
+    }
+}
---
+++ eAgenda.Tests/CompromissoModule/ControladorCompromissoTest.cs
@@ -0,0 +1,105 @@
+﻿using eAgenda.Controladores.CompromissoModule;
+using eAgenda.Controladores.ContatoModule;
+using eAgenda.Controladores.Shared;
+using eAgenda.Dominio.CompromissoModule;
+using eAgenda.Dominio.ContatoModule;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using FluentAssertions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace eAgenda.Tests.CompromissoModule
+{
+    [TestClass]
+    public class ControladorCompromissoTest
+    {
+        ControladorCompromisso controladorCompromisso;
+        ControladorContato controladorContato;
+        public ControladorCompromissoTest()
+        {
+            controladorCompromisso = new ControladorCompromisso();
+            controladorContato = new ControladorContato();
+            Db.Update("DELETE FROM [TBCOMPROMISSO]");
+            Db.Update("DELETE FROM [TBCONTATO]");
+
+            Db.Update("DELETE FROM [TBCONTATO]");
+            var novoContato = new Contato("José Pedro", "jose.pedro@gmail.com", "321654987", "JP Ltda", "Dev");       
+            controladorContato.InserirNovo(novoContato);
+            
+        }
+
+        [TestMethod]
+        public void Inserindo()
+        {
+            Contato contatoEncontrado = controladorContato.SelecionarTodos()[0];
+            Compromisso compromisso = new Compromisso("aa", "Casa", "aaa", DateTime.Today, new TimeSpan(), new TimeSpan(), contatoEncontrado);
+            controladorCompromisso.InserirNovo(compromisso);
+            Compromisso compromissoEncontrado = controladorCompromisso.SelecionarTodos()[0];
+
+            compromissoEncontrado.Should().Be(compromisso);     
+            
+        }
+        [TestMethod]
+        public void Editar()
+        {
+            Contato contatoEncontrado = controladorContato.SelecionarTodos()[0];
+            Compromisso compromisso = new Compromisso("aa", "Casa", "aaa", DateTime.Today, new TimeSpan(), new TimeSpan(), contatoEncontrado);
+            controladorCompromisso.InserirNovo(compromisso);
+            Compromisso compromisso1 = new Compromisso("bb", "Casa", "aaa", DateTime.Today, new TimeSpan(), new TimeSpan(), contatoEncontrado);
+            controladorCompromisso.Editar(compromisso.Id,compromisso1);
+
+            Compromisso compromissoEncontrado = controladorCompromisso.SelecionarTodos()[0];
+
+            compromissoEncontrado.Should().Be(compromisso1);
+          
+        }
+        [TestMethod]
+        public void Excluir()
+        {
+            Contato contatoEncontrado = controladorContato.SelecionarTodos()[0];
+            Compromisso compromisso = new Compromisso("aa", "Casa", "aaa", DateTime.Today, new TimeSpan(), new TimeSpan(), contatoEncontrado);
+            controladorCompromisso.InserirNovo(compromisso);
+            controladorCompromisso.Excluir(compromisso.Id) ;
+
+            controladorCompromisso.SelecionarTodos().Should().HaveCount(0);           
+        }
+        [TestMethod]
+        public void SelecionaFuturos()
+        {
+            Contato contatoEncontrado = controladorContato.SelecionarPorId(1);
+            Compromisso compromisso = new Compromisso("aa", "Casa", "aaa", DateTime.Today.AddDays(10), new TimeSpan(), new TimeSpan(), contatoEncontrado);
+            controladorCompromisso.InserirNovo(compromisso);
+            
+            controladorCompromisso.SelecionarCompromissosFuturos(DateTime.Today,DateTime.Today.AddDays(12)).Should().HaveCount(1);
+        }
+        [TestMethod]
+        public void SelecionaPassados()
+        {
+            Contato contatoEncontrado = controladorContato.SelecionarPorId(1);
+            Compromisso compromisso = new Compromisso("aa", "Casa", "aaa", new DateTime(2021,01,01), new TimeSpan(), new TimeSpan(), contatoEncontrado);
+            controladorCompromisso.InserirNovo(compromisso);
+
+            controladorCompromisso.SelecionarCompromissosPassados(DateTime.Now).Should().HaveCount(1);
+        }
+        [TestMethod]
+        public void NaoDeveInserir_NaMesmaDataEHora()
+        {
+            //arrange
+            Compromisso compromisso1 = new Compromisso("Testar", "Casa", "", new DateTime(2001,07,03), new TimeSpan(13,00,00), new TimeSpan(14,00,00), null);
+            controladorCompromisso.InserirNovo(compromisso1);
+
+            Compromisso compromissoInvalido = new Compromisso("Testar", "Casa", "", new DateTime(2001, 07, 03), new TimeSpan(13, 00, 00), new TimeSpan(14, 00, 00), null);
+            //act
+            string resultado = controladorCompromisso.InserirNovo(compromissoInvalido);
+
+            //assert
+            resultado.Should().Be("Já há compromisso marcado nessa data e horário");
+            controladorCompromisso.SelecionarTodos().Should().HaveCount(1);
+        }
+        
+    }
+}
